608f8b20334731d6fe3ac6701d2107b15af0ff66
ashokvpi99 <ashokvpi99@gmail.com>
2018-09-25 23:46:36 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Hi All,
+
+React demo with basic installation
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  render() {
+    return (
+      <React.Fragment>
+        <h2>Hello World</h2>
+      </React.Fragment>
+    )
+  }
+
+}
+
+export default App;

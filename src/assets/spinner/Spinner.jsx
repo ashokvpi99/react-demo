8979adbdc39824b31481b3c481312370a31d18d2
ashokvpi99 <ashokvpi99@gmail.com>
2018-10-03 01:47:56 +0530

    spinner changed

--- src/assets/spinner/Spinner.jsx
+++ src/assets/spinner/Spinner.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import Loader from 'react-loader-spinner'
 
-const Spinner = (props) => {
-    const isActive  = props.active ? <div style={{marginLeft: 500}}><Loader type="ThreeDots" color="blue" height={80} width={80} /></div>: null;
-    return isActive;
-};
+const Spinner = (props) => props.active ? <div style={{marginLeft: 500}}><Loader type="Triangle" color="orange" height={80} width={80} /></div>: null;
 
 export default Spinner;

ab7703b26b6145a80f750918b73862ae489c5aeb
midhun e <midhun.e@photoninfotech.net>
2018-10-05 15:45:13 +0530

    fresh

--- src/App.js
+++ src/App.js
@@ -1,48 +1,11 @@
 import React, { Component } from 'react';
-import Todos from './components/Todos';
-import AddTodo from './components/AddTodo';
 
 class App extends Component {
-
-  constructor() {
-    super();
-    
-    this.state = {
-      todos: []
-    };
-
-  }
-
-  addTodo = (todo) => {
-    todo.id = Math.floor(Math.random() * 10);
-    let todos = [...this.state.todos, todo]
-    this.setState({
-      todos: todos
-    })
-  }
-
-  deleteTodo = (id) => {
-    let remTodos = this.state.todos.filter((todo) => todo.id !== id);
-    this.setState({
-      todos: remTodos
-    })
-  }
 
   render() {
     return (
       <React.Fragment>
-        <h2 className={'center'}>Todos</h2>
-        <br/>
-        <div className={'container'}>
-          {
-            this.state.todos.length ? <span className="new badge blue" data-badge-caption="remaing">{this.state.todos.length}</span> :
-            <span className="new badge green" data-badge-caption="All caguhtup"><i className="tiny material-icons">done_all</i></span>
-          }
-          <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />  
-        </div>
-        <br/>
-        <br/>
-        <AddTodo addTodo={this.addTodo} />
+        <p>Hello</p>
       </React.Fragment>
     )
   }

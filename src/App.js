7f584c5d8de9d1952ad0dc30e177de5c5f945ea5
midhun e <midhun.e@photoninfotech.net>
2018-10-05 16:25:10 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
 
   render() {
     return (
-      <React.Fragment>
-        <p>Hello</p>
-      </React.Fragment>
+      <BrowserRouter>
+        <React.Fragment>
+          <NavBar/>
+          <Switch>
+            <Route component={Home} path={'/'}></Route>
+            <Route component={Register} path={'/register'}></Route>
+            <Route component={Login} path={'/login'}></Route>
+          </Switch>
+        </React.Fragment>
+      </BrowserRouter>
     )
   }
 

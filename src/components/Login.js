521bad97e5e642ff7b0c40e0e473c804883b138c
ashokvpi99 <ashokvpi99@gmail.com>
2018-10-06 21:25:55 +0530

    form added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            psw: ''
+        };
+    }
+
+    onChangeSetState = (e) => {
+        const { name, value } = e.target;
+        this.setState({
+            [name]: value
+        })
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('this.state...', this.state);
+    };
     render() {
         return (
             <React.Fragment>
@@ -8,20 +28,20 @@ class Login extends Component {
                 <div className="container">
                     <div className="col s12 m5">
                         <div className="card-panel">
-                            <form>
+                            <form onSubmit={this.handleSubmit}>
                                 <div className="container">
                                     <h1>Login</h1>
                                     <p>Please fill the details to login to your account.</p>
                                     <hr />
                                     <div className="row">
                                         <div className="input-field col s8">
-                                            <input type="text" name="email" required />
+                                            <input type="text" name="email" onChange={this.onChangeSetState} required />
                                             <label htmlFor="email">Email</label>
                                         </div>
                                     </div>
                                     <div className="row">
                                         <div className="input-field col s8">
-                                            <input type="password" name="psw" required />
+                                            <input type="password" onChange={this.onChangeSetState} name="psw" required />
                                             <label htmlFor="psw">Password</label>
                                         </div>
                                     </div>
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,6 +2,35 @@ import React, { Component } from "react";
 
 class Signup extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: '',
+            gender: 'male',
+            dob: '' || new Date(),
+            mobileNo: '',
+            email: '',
+            psw: '',
+            pswrepeat: ''
+        };
+    }
+
+    onChangeState = (e) => {
+        var { name, value, checked } = e.target;
+        if(name === 'gender') {
+            value = checked ? 'female' : 'male';
+        }
+        this.setState({
+            [name]: value
+        });
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('this.state......', this.state)
+    };
+
     render() {
         return (
             <React.Fragment>
@@ -9,53 +38,56 @@ class Signup extends Component {
                 <div className="container">
                     <div className="col s12 m5">
                         <div className="card-panel">
-                            <form>
+                            <form onSubmit={this.handleSubmit}>
                                 <div className="container">
                                     <h1>Register</h1>
                                     <p>Please fill in this form to create an account.</p>
                                     <hr />
                                     <div className="row">
                                         <div className="input-field col s10">
-                                            <input type="text" name="name" required />
+                                            <input type="text" name="name" onChange={this.onChangeState} required />
                                             <label htmlFor="name">Name</label>
                                         </div>
                                     </div>
                                     <div className="row">
-                                        <div className="input-field col s10" style={{marginLeft: '-13px'}}>
-                                            <input type="radio" name="gender" defaultChecked/>
-                                            <label htmlFor="always">Male</label>
-                                            &nbsp;&nbsp;
-                                            <input type="radio" name="gender" />
-                                            <label htmlFor="never">Female</label>
+                                        <div className="input-field col s10">
+                                            <div className="switch">
+                                                <label>
+                                                    Male
+                                                    <input name={'gender'} onChange={this.onChangeState} type="checkbox" />
+                                                        <span className="lever"></span>
+                                                        Female
+                                                </label>
+                                            </div>
                                         </div>
                                     </div>
                                     <div className="row">
                                         <div className="input-field col s10">
-                                            <input type="text" className="datepicker" />
+                                            <input type="text" name={'dob'} onSelect={this.onChangeState} className="datepicker" />
                                             <label htmlFor="dob">Date of Birth</label>
                                         </div>
                                     </div>
                                     <div className="row">
                                         <div className="input-field col s10">
-                                            <input type="text" name="phoneno" required />
-                                            <label htmlFor="phoneno">Phone Number</label>
+                                            <input type="text" onChange={this.onChangeState} name="mobileNo" required />
+                                            <label htmlFor="phoneno">Mobile Number</label>
                                         </div>
                                     </div>
                                     <div className="row">
                                         <div className="input-field col s10">
-                                            <input type="text" name="email" required />
+                                            <input type="text" onChange={this.onChangeState} name="email" required />
                                             <label htmlFor="email">Email</label>
                                         </div>
                                     </div>
                                     <div className="row">
                                         <div className="input-field col s10">
-                                            <input type="password" name="psw" required />
+                                            <input type="password" onChange={this.onChangeState} name="psw" required />
                                             <label htmlFor="psw">Password</label>
                                         </div>
                                     </div>
                                     <div className="row">
                                         <div className="input-field col s10">
-                                            <input type="password" name="pswrepeat" required />
+                                            <input type="password" onChange={this.onChangeState} name="pswrepeat" required />
                                             <label htmlFor="psw-repeat">Repeat Password</label>
                                         </div>
                                     </div>

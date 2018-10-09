290bf3e5756b93eeffc36b0b0e0dda295ab78787
midhun e <midhun.e@photoninfotech.net>
2018-10-09 21:13:37 +0530

    spinner color updated

--- src/assets/spinner/Spinner.jsx
+++ src/assets/spinner/Spinner.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Loader from 'react-loader-spinner'
 
-const Spinner = (props) => props.active && <div className={'center'}><Loader type="Triangle" color="blue" height={80} width={80} /></div>;
+const Spinner = (props) => props.active && <div className={'center'}><Loader type="Triangle" color="#26a69a" height={80} width={80} /></div>;
 
 export default Spinner;
